19cc4e721e4235c635ab9ac500e35b8816295d08
jipo <zhipengsmith@gmail.com>
2012-04-13 17:58:34 -0400

    added TrackControllerLib-Gilbert to the solution.

---
+++ TrackControllerLib-Gilbert/TrackController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommonLib;
+using TrackControlLib.Gilbert;
+
+namespace TrackControllerLib
+{
+    namespace Gilbert
+    {
+        public class TrackController : ITrackController
+        {
+            #region Private Data
+            private Dictionary<string, TrackBlock> m_trackBlocks;
+            #endregion
+
+            #region Public Methods
+
+            //TODO: Add a constructor
+
+            // METHOD: checkTrackBlockExistence
+            // - Check track block existence with given trackId
+            public bool checkTrackBlockExistence(string trackId)
+            {
+                if (!m_trackBlocks.ContainsKey(trackId))
+                    throw new KeyNotFoundException();
+                return true;
+            }
+
+            // METHOD: setAuthority
+            // - Set track authority by trackId
+            public bool setAuthority(string trackId, BlockAuthority auth)
+            {
+                if (checkTrackBlockExistence(trackId))
+                {
+                    m_trackBlocks[trackId].Authority = auth;
+                    return true;
+                }
+                return false;
+            }
+
+            // METHOD: closeTrack
+            // - Close track by trackId
+            public bool closeTrack(string trackId)
+            {
+                if (checkTrackBlockExistence(trackId))
+                {
+                    m_trackBlocks[trackId].Status.IsOpen = false;
+                    return true;
+                }
+                return false;
+            }
+
+            // METHOD: openTrack
+            // - Open track by trackId
+            public bool openTrack(string trackId)
+            {
+                if (checkTrackBlockExistence(trackId))
+                {
+                    m_trackBlocks[trackId].Status.IsOpen = true;
+                    return true;
+                }
+                return false;
+            }
+
+            // METHOD: isTrackClosed
+            // - Check track open status by trackId
+            public bool isTrackClosed(string trackId)
+            {
+                if (checkTrackBlockExistence(trackId))
+                    return !(m_trackBlocks[trackId].Status.IsOpen);
+                return true;
+            }
+
+            // METHOD: getTrackStatus
+            // - Retrieve track status by trackId
+            public TrackStatus getTrackStatus(string trackId)
+            {
+                if (checkTrackBlockExistence(trackId))
+                    return m_trackBlocks[trackId].Status;
+                return null;
+            }
+
+            #endregion
+        }
+    }
+}
