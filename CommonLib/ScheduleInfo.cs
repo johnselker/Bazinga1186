8e05af3f9b68e4dd68d13380360bb4c72c126fdd
John Selker <jes139@pitt.edu>
2012-04-18 19:52:32 -0400

    Added ScheduleInfo class and updated TrainController.

---
+++ CommonLib/ScheduleInfo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonLib
+{
+    public class ScheduleInfo
+    {
+        #region Private Data
+
+        private string m_stationName;
+        private double m_timeToStation;
+
+        #endregion
+
+        #region Accessors
+
+        // ACCESSOR: StationName
+        //--------------------------------------------------------------------------------------
+        /// <summary>
+        /// Name of the station
+        /// </summary>
+        //--------------------------------------------------------------------------------------
+        public String StationName
+        {
+            get { return m_stationName; }
+        }
+
+        // ACCESSOR: TimeToStationMinutes
+        //--------------------------------------------------------------------------------------
+        /// <summary>
+        /// Total time to the station in minutes
+        /// </summary>
+        //--------------------------------------------------------------------------------------
+        public double TimeToStationMinutes
+        {
+            get { return m_timeToStation; }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        // METHOD: ScheduleInfo
+        //--------------------------------------------------------------------------------------
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// 
+        /// <param name="stationName">Name of the station</param>
+        /// <param name="timeToStation">Total time to the station in minutes</param>
+        //--------------------------------------------------------------------------------------
+        public ScheduleInfo(string stationName, double timeToStation)
+        {
+            m_stationName = stationName;
+            m_timeToStation = timeToStation;
+        }
+
+        #endregion
+    }
+}
