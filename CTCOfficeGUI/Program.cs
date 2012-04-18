5262ebe239ecda9abb220111093f62aa247ed6ed
Jeremy Nelson <jernelson7@gmail.com>
2012-04-18 17:03:33 -0400

    Added some exception handling

--- CTCOfficeGUI/Program.cs
+++ CTCOfficeGUI/Program.cs
@@ -16,6 +16,33 @@ namespace CTCOfficeGUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainScreen());
+            System.AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+        }
+
+        /// <summary>
+        /// Event handler for unhandled exceptions
+        /// </summary>
+        /// <param name="sender">Sender of the event</param>
+        /// <param name="e">Event arguments</param>
+        private static void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                Exception ex = (Exception)e.ExceptionObject;
+                CommonLib.LoggingTool logger = new CommonLib.LoggingTool(System.Reflection.MethodBase.GetCurrentMethod());
+                logger.LogError("Unhandled Exception", ex);
+
+                MessageBox.Show("Something bad happened. Check the log files for more information");
+            }
+            catch
+            {
+                //Can't do anything
+            }
+            finally
+            {
+                //Nothing else we can do at this point. Just die.
+                Application.Exit();
+            }
         }
     }
 }
