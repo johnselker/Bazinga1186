e3e9b9bc564ea2ea2c1b054d59ea53d682854656
John Selker <jes139@pitt.edu>
2012-04-19 16:55:40 -0400

    Added ITrainController interface.

---
+++ TrainControlLib/ITrainController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommonLib;
+
+namespace TrainControllerLib
+{
+    class ITrainController
+    {
+        #region Public Methods
+        // METHOD: Update
+        //--------------------------------------------------------------------------------------
+        /// <summary>
+        /// Call the train contoller
+        /// </summary>
+        /// <param name="dt">Delta time</param>
+        //--------------------------------------------------------------------------------------
+        public void Update(double dt) {}
+
+        // METHOD: SetSchedule
+        //--------------------------------------------------------------------------------------
+        /// <summary>
+        /// Set the schedule
+        /// </summary>
+        //--------------------------------------------------------------------------------------
+        public void SetSchedule(Queue<ScheduleInfo> routeInfo) {}
+
+        #endregion
+    }
+}
